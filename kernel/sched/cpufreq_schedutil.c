11bcfb753c78e0a3adeb8c8c81fb1917dd010a89
AbhinandAK350 <abhinandak@outlook.com>
2020-02-22 10:30:27 +0530

    fix error

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -414,7 +414,7 @@ static void sugov_update_single(struct update_util_data *hook, u64 time,
 		 * recently, as the reduction is likely to be premature then.
 		 */
 		if (busy && next_f < sg_policy->next_freq &&
-		    sg_policy->next_freq != UINT_MAX) {
+		    sg_policy->next_freq != UINT_MAX)
 			next_f = sg_policy->next_freq;
 	}
 
