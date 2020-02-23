5971ad02b40d78ec4145af2887b8676872dd87b2
AbhinandAK350 <abhinandak@outlook.com>
2020-02-23 15:03:52 +0530

    sched/fair: prefer energy efficient CPUs for !prefer_idle tasks

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7298,7 +7298,7 @@ retry:
 				 * IOW, prefer a deep IDLE LITTLE CPU vs a
 				 * shallow idle big CPU.
 				 */
-				if (sysctl_sched_cstate_aware &&
+				if (capacity_orig >= target_capacity &&
 				    sysctl_sched_cstate_aware &&
 				    best_idle_cstate <= idle_idx)
 					continue;
@@ -7341,7 +7341,7 @@ retry:
 			active_cpus_count++;
 
 			/* Favor CPUs with maximum spare capacity */
-			if (capacity_orig == target_capacity &&
+			if (capacity_orig >= target_capacity &&
 			    (capacity_orig - new_util) < target_max_spare_cap)
 				continue;
 
